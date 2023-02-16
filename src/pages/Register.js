75d86a302ec155bbb17598695367f4e267c6fb9c
maniyadg <maniyarasandhanasekaran@gmail.com>
2023-02-16 13:16:20 +0530

    final route added

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -75,7 +75,7 @@ export default function Register() {
           process.env.REACT_APP_LOCALHOST_KEY,
           JSON.stringify(data.user)
         );
-        navigate("/");
+        navigate("/setAvatar");
       }
     }
   };
